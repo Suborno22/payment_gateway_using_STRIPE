817b7857be1207f28fcd9bb703a6ec693da8af71
Suborno22 <dsuborno0@gmail.com>
2023-11-27 00:44:24 +0530

    removed docker

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $username = "root";
 $password = "password";
 $host = "localhost";
 $database = "Dresser";
-$connect = new mysqli($host,$username,$password,$database);
+$connect = mysqli_connect($host,$username,$password,$database);
 if(mysqli_connect_errno()){
     echo "".mysqli_connect_error();
 }
